01af39ba8138a12256322b30961345e3709470f2
Prashant <prashantk.tps@gmail.com>
2020-08-31 11:17:08 +0530

    add project section

---
+++ src/assets/data/projectCardData.ts
@@ -0,0 +1,39 @@
+import project1 from '../images/project1.png';
+import project2 from '../images/project2.png';
+import project3 from '../images/project3.png';
+import project4 from '../images/project4.png';
+
+const projectCardData = [
+  {
+    heading: 'Bridg.',
+    para:
+      'Bridg is an enterprise SaaS software platform designed for brick and mortar retailers, cpg brands, cinemas, restaurants, departmental stores etc.',
+    color: '#6C91D8',
+    img: project1,
+    lefty: true,
+  },
+  {
+    heading: 'Cardz',
+    para:
+      'Web Exploration of Trust Bank Credit cards. Cardz can help you get the best fit credit card for you daily lives with just a single click.',
+    color: '#FC6C4E',
+    img: project2,
+    lefty: false,
+  },
+  {
+    heading: 'Travel',
+    para:
+      'Travel. - is an intelligent, AI-based travel app that helps you organize, book, and track your trips. Earn travel points each time and spend 100% on your next flight.',
+    color: '#502FFF',
+    img: project3,
+    lefty: true,
+  },
+  {
+    heading: 'Crowdfunding',
+    para: `I'm working on an exploration of the Crowdfunding Landing page. Take a look at the design of this awesome landing page!`,
+    color: '#3BB273',
+    img: project4,
+    lefty: false,
+  },
+];
+export default projectCardData;
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -5,11 +5,13 @@ import styles from './home.module.css';
 import Hero from '../../components/hero/Hero';
 import Services from '../../components/services/services';
 import Footer from '../../components/footer/Footer';
+import ProjectSection from '../../components/projectSection/projectSection';
 
 const Home: FC = () => {
   return (
     <div className={styles.home}>
       <Hero />
+      <ProjectSection />
       <Services />
       <Footer />
     </div>
